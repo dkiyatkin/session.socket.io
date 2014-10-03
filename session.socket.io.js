4c05e52414644de11973e124873e10950e94db8f
Dmitriy Kiyatkin <d.kiyatkin@itlf.ru>
2014-10-03 21:48:07 +0400

    getSession after connection

--- session.socket.io.js
+++ session.socket.io.js
@@ -29,9 +29,10 @@ function SessionSockets(io, sessionStore, cookieParser, key) {
 
   function bind(event, callback, namespace) {
     namespace.on(event, function (socket) {
-      sessionSockets.getSession(socket, function (err, session) {
-        callback(err, socket, session);
-      });
+      // sessionSockets.getSession(socket, function (err, session) {
+      //   callback(err, socket, session);
+      // });
+      callback(socket, sessionSockets.getSession);
     });
   }
 
